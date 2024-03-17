18d5ab9e613ce22fe6209c7b28b817fd86ed32b8
Apple <apple@Aana-MacBook-Pro.local>
2024-03-17 14:25:48 +0600

    section

---
+++ src/components/BannerSec/Banner.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Banner.css'
+
+const Banner = () => {
+  return (
+    <div>
+        <div className='text'>
+            <h5 className='texth5'>T-shirt / Tops</h5>
+            <h1 className='text-h1'>Spring <br /> Value Package</h1>
+            <h4 className='text-h4'>cool / colorful / comfy</h4>
+            <button className='btnbl'>Shop Now</button>
+        </div>
+    </div>
+  )
+}
+
+export default Banner
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from '../header/Header'
 import Footer from '../Footer/Footer'
+import Banner from '../BannerSec/Banner'
+import BannerYell from '../BannerYel/BannerYell'
 
 function Layout() {
   return (
     <div>
       <Header />
       <Outlet />
+      <Banner/>
+      <BannerYell/> 
       <Footer />
     </div>
   )
